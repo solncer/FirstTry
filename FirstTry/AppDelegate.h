719f9a51feb761d389aab65e7fc5db33ca210807
Admin <krutova@yandex.ru>
2015-05-19 11:01:58 +0300

    Initial commit

---
+++ FirstTry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstTry
+//
+//  Created by Admin on 19.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstTry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstTry
+//
+//  Created by Admin on 19.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
